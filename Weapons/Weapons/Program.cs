40c625ee3277496471b528426084212d9277c927
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-04-07 22:37:27 +0400

    Weapon

---
+++ Weapons/Weapons/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Weapons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Daragin gulle tutumunu daxil edin:");
+            int capacity = int.Parse(Console.ReadLine());
+            Weapon weapon = new Weapon(capacity);
+
+            while (true)
+            {
+                Console.WriteLine("Secim edin: 0 - Melumat, 1 - Atıs, 2 - Gulle Sayi, 3 - Yeniden Doldur, 4 - Mod Dəyis, 5 - cixis");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Daragi gulle tutumu: " + capacity + ", Cari gulle sayi: " + weapon.GetRemainBulletCount());
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine("Daragin dolmasi ücün lazim olan gulle sayi: " + weapon.GetRemainBulletCount());
+                        break;
+                    case 3:
+                        Console.WriteLine("Yeniden doldurmaq ucun gulle sayini daxil edin:");
+                        int bullets = int.Parse(Console.ReadLine());
+                        weapon.Reload(bullets);
+                        break;
+                    case 4:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 5:
+                        Console.WriteLine("Proqramdan cixilir...");
+                        return;
+                    default:
+                        Console.WriteLine("Yanlis secim, yeniden cehd edin.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Weapons/Weapons/Weapon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapons
+{
+    public class Weapon
+    {
+        private int bulletCapacity;
+        private int currentBullets;
+        private bool isAutomatic;
+
+        public Weapon(int capacity)
+        {
+            bulletCapacity = capacity;
+            currentBullets = 0;
+            isAutomatic = false;
+        }
+
+        public void Shoot()
+        {
+            if (currentBullets > 0)
+            {
+                if (isAutomatic)
+                {
+                    Console.WriteLine("Avtomatik atis: " + currentBullets + " gulle atildi.");
+                    currentBullets = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Tekli atis:1gulle atildi.");
+                    currentBullets--;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Gulle qalmayib!");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return bulletCapacity - currentBullets;
+        }
+
+        public void Reload(int bullets)
+        {
+            if (currentBullets + bullets <= bulletCapacity)
+            {
+                currentBullets += bullets;
+                Console.WriteLine(bullets + "gulle elave edildi.Cari gulle sayi " + currentBullets);
+            }
+            else
+            {
+                Console.WriteLine("Oops");
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            isAutomatic = !isAutomatic;
+            string mode = isAutomatic ? "Avtomatik" : "Tekli";
+            Console.WriteLine("Atış modu deyisdi: " + mode);
+        }
+    }
+}
+
+
+
